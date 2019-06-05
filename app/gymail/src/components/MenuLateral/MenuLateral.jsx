9a977ef8b307cc0d2cc64048d0db1c7d6e0d1463
Yasmin Moraesm <yasminms46@gmail.com>
2019-06-04 22:50:35 -0300

    feat: adicionando o componente menu lateral

---
+++ app/gymail/src/components/MenuLateral/MenuLateral.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+import './MenuLateral.css'
+
+export default class MenuLateral extends Component {
+    render() {
+        return (
+            <div className="menu-lateral-container">
+                
+            </div>
+        )
+    }
+}
