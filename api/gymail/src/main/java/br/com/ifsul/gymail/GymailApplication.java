9f96eca502e540b769ee78e2fe1799df2ec52e7c
Guilherme Francisco <guisanfrancisco@gmail.com>
2019-06-07 11:13:16 -0300

    configurando swagger-ui

--- api/gymail/src/main/java/br/com/ifsul/gymail/GymailApplication.java
+++ api/gymail/src/main/java/br/com/ifsul/gymail/GymailApplication.java
@@ -4,9 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 @EnableSwagger2
 @SpringBootApplication
@@ -21,7 +25,18 @@ public class GymailApplication {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("br.com.ifsul.gymail"))
-                .build();
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "GyMail - API",
+                "Serviço referente ao sistema de troca de mensagens. DSV: http://gymail-api.herokuapp.com/api/gymail/swagger-ui.html",
+                "API 1.0",
+                "Terms of service",
+                new Contact("Guilherme dos Santos Francisco e Yasmin Moraes da Silva", "http://gymail-api.herokuapp.com/api/gymail/swagger-ui.html", ""),
+                "License of API", "API license URL", Collections.emptyList());
     }
 
 }
